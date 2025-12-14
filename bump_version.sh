f8187f1c1edc4b5d06f702fe328ad93941f25e1f
Dan Shumaker <danpshumaker@gmail.com>
2025-12-13 19:05:35 -0500

    get order right

--- bump_version.sh
+++ bump_version.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 DATE=$(date "+%Y-%m-%d_%H_%M_%S")
-
-echo "$DATE" >VERSION
-
-sed -i "s/version \".*\"/version \"${DATE}\"/" Formula/denver.rb
-
 git fetch origin main
 git rebase origin/main
+echo "$DATE" >VERSION
+sed -i "s/version \".*\"/version \"${DATE}\"/" Formula/denver.rb
 git add VERSION Formula/denver.rb
 git commit -m "Bump version to ${DATE}"
 git tag -f "${DATE}"
